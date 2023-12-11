4686cab78bcd2f7cf1c548e213d38c5a3fcae232
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2023-12-11 01:10:37 -0400

    adicionando os liks

--- PTAC-projeto-final-main/src/main.jsx
+++ PTAC-projeto-final-main/src/main.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './home/Home'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './home/Home';
+import Detalhe from './detalhe/Detalhe'; // Importe a página Detalhe
+import Destaques from './destaques/Destaques'; // Importe a página Destaques
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/detalhe" element={<Detalhe />} /> {/* Rota para a página Detalhe */}
+        <Route path="/destaques" element={<Destaques />} /> {/* Rota para a página Destaques */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
