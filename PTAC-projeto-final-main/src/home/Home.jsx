d19d0c4d4a8a79d43ef7edb77acd6d2e05607a84
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2023-12-10 21:56:57 -0400

    arrumando o css horroroso

--- PTAC-projeto-final-main/src/home/Home.jsx
+++ PTAC-projeto-final-main/src/home/Home.jsx
@@ -1,11 +1,11 @@
-
+// home.jsx
 import "./home.css";
 
 export default function Home() {
   return (
     <div className="navbar">
       <div className="logo"></div>
-      <div className="login-icon">Login</div>
+      <button className="login-button">Entrar</button>
     </div>
   );
 }
