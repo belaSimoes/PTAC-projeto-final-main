61e53585ba5ee37f93f3d63a60dba07fef2a745b
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2023-12-11 23:15:25 -0400

    arrumando destaque

--- PTAC-projeto-final-main/src/Destaque.jsx
+++ PTAC-projeto-final-main/src/Destaque.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import Card from './Card';
-import './styles.css'; // Seu arquivo CSS com as estilizações necessárias
 
 export default function Destaque() {
-    const data = JSON.parse(localStorage.getItem('Lista')) || [];
+    const pegarvideos = JSON.parse(localStorage.getItem('Lista')) || [];
+
+    // Estilos diretamente no componente
+    const containerStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '20px',
+    };
+
+    const sidebarStyle = {
+        width: '200px',
+        
+    };
+
+    const valuesAreaStyle = {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        flexWrap: 'wrap',
+        
+    };
 
     return (
-        <div className="container">
-            <nav className="sidebar">
+        <div style={containerStyle}>
+            <div style={sidebarStyle}>
                 <ul>
-                    <li><a href="/">Página Principal</a></li>
+                    <li><a href="/">Home</a></li>
                     <li><a href="/destaque">Em Destaque</a></li>
-                    <li><a href="/registro">Novo Registro</a></li>
+                    <li><a href="/registro">Novo Vídeo</a></li>
                 </ul>
-            </nav>
-            <div className="values-area">
-                {data.slice(-4).map((item, index) => (
+            </div>
+
+            <div style={valuesAreaStyle}>
+                {pegarvideos.slice(-4).map((item, index) => (
                     <div className="card" key={item.id}>
                         <Card item={item} />
                     </div>
